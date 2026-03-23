fbfe9549dc3e8bf36f50c675e9996ca5c7d6ee4b
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2026-03-23 19:11:55 +0100

    añadir modelos de direcciones, favoritos y carrito de compras

--- Moda/Moda/admin.py
+++ Moda/Moda/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Marca, Producto
 
-# Register your models here.
+# Registramos los modelos para que aparezcan en el panel web
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Producto)
