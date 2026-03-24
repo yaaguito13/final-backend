375bcfdebcabd4376f163141482e874c3c55e2d2
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2026-03-24 10:40:13 +0100

    endpoint de catalogo y marcas con soporte para imagenes

--- Moda/modaeats_backend/settings.py
+++ Moda/modaeats_backend/settings.py
@@ -117,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+import os
+
+# Configuración para subir imágenes
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- Moda/modaeats_backend/urls.py
+++ Moda/modaeats_backend/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from Moda import views # Importamos las vistas de tu app Moda
+from Moda import views
+from django.conf import settings # <-- NUEVO
+from django.conf.urls.static import static # <-- NUEVO
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Rutas de autenticación
     path('api/registro/', views.registro_usuario, name='registro'),
     path('api/login/', views.login_usuario, name='login'),
+    path('api/marcas/', views.lista_marcas, name='marcas'),
+    path('api/productos/', views.catalogo_productos, name='productos'),
 ]
+
+# <-- NUEVO: Solo para entorno de desarrollo, permite servir los archivos multimedia
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
