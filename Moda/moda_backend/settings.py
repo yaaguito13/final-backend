22afc2caceb8e23331c5f298569208790d7114b0
Yago Pazos Lema <yagopazoslema@MacBook-Air-de-Yago.local>
2026-05-06 13:10:37 +0200

    Arreglo al iniciar server

--- Moda/moda_backend/settings.py
+++ Moda/moda_backend/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
